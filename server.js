6594884aa966dd6d0f491803de3f0a0ef2eef200
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-13 13:15:48 -0400

    Updating with the db

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+var db
+
+MongoClient.connect('mongodb://xchangrey:Xchan%401091!@ds137661.mlab.com:37661/quotes-app', {useNewUrlParser: true},(err, client) => {
+if(err) return console.log(err)
+db = client.db('quotes-app');
+app.listen(process.env.PORT || 3000, function(){
+	console.log('hooray! server running on port 3000');
+});
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => res.sendFile(__dirname+"/index.html"));
 
-app.listen(3000, function(){
-	console.log('hooray! server running on port 3000');
-});
+app.post('/quotes', (req, res) => console.log(req.body));
